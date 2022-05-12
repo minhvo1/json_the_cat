5c36e17addea1b473a66a3696cc8a59c7d76e08c
minhvo1 <minhvo199@gmail.com>
2022-05-12 22:26:36 +0000

    added tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,13 @@ const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   const data = JSON.parse(body);
 
-  if (error) {
-    callback(error, null);
+
+  if (data[0] === undefined) {
+    callback("No breed found for this query", null);
   } else {
-    if (data[0] === undefined) {
-      callback(null, "No breed found for this query");
-    } else {
-      callback(null, data[0].description);
-    }
+    callback(null, data[0].description);
   }
+  
 
 });
 };
